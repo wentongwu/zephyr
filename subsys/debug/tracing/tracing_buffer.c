43b2b09f80460b118f67ced7c13c4fcbc540b3c4
Wentong Wu <wentong.wu@intel.com>
2019-12-11 06:19:50 +0800

    async and sync tracing
    Signed-off-by: Wentong Wu <wentong.wu@intel.com>

--- subsys/debug/tracing/tracing_buffer.c
+++ subsys/debug/tracing/tracing_buffer.c
@@ -6,9 +6,6 @@
 
 #include <sys/ring_buffer.h>
 
-static u32_t total_size;
-static struct ring_buf tracing_ring_buf;
-static u8_t tracing_buffer[CONFIG_TRACING_BUFFER_SIZE];
 static u8_t tracing_cmd_buffer[CONFIG_TRACING_CMD_BUFFER_SIZE];
 
 #ifdef CONFIG_NEWLIB_LIBC
@@ -17,6 +14,11 @@ extern void z_vprintk(out_func_t out, void *ctx, const char *fmt, va_list ap);
 #else
 extern int z_prf(int (*func)(), void *dest, char *format, va_list vargs);
 #endif
+
+#ifdef CONFIG_TRACING_ASYNC
+static u32_t total_size;
+static struct ring_buf tracing_ring_buf;
+static u8_t tracing_buffer[CONFIG_TRACING_BUFFER_SIZE];
 
 static int str_out(int c, void *ctx)
 {
@@ -99,6 +101,43 @@ u32_t tracing_buffer_capacity_get(void)
 {
 	return ring_buf_capacity_get(&tracing_ring_buf);
 }
+#endif
+
+#ifdef CONFIG_TRACING_SYNC
+static u32_t length;
+static u8_t tracing_buffer[CONFIG_TRACING_PACKET_BUF_SIZE];
+
+static int str_out(int c, void *ctx)
+{
+	tracing_buffer[length] = (u8_t)c;
+	length++;
+
+	return 0;
+}
+
+bool tracing_buffer_str_put(const char *str, va_list args)
+{
+	length = 0;
+#if !defined(CONFIG_NEWLIB_LIBC) && !defined(CONFIG_ARCH_POSIX)
+	(void)z_prf(str_out, NULL, (char *)str, args);
+#else
+	z_vprintk(str_out, NULL, str, args);
+#endif
+
+	/*
+	 * TODO put tracing_buffer directly to backend. Need to think about API.
+	 * Known that this function is protected by irq_lock and irq_unlock.
+	 */
+}
+
+bool tracing_buffer_put(u8_t *data, u32_t size)
+{
+	/*
+	 * TODO put data directly to backend. Need to think about API.
+	 * Known that this function is protected by irq_lock and irq_unlock.
+	 */
+}
+#endif
 
 u8_t *tracing_cmd_buffer_alloc(void)
 {
